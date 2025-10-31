2e379e3000da5f84fcd56a2aee7e18e253e67c31
y3rbiadit0 <yerbiadito@gmail.com>
2025-10-31 10:12:01 +0100

    Add support for python 3.10 (StrEnum)

--- src/infra_lib/infra/enums.py
+++ src/infra_lib/infra/enums.py
@@ -1,6 +1,9 @@
-from enum import StrEnum
+from enum import Enum
 
-
+class StrEnum(str, Enum):
+    def __str__(self) -> str:
+        return str(self.value)
+    
 class InfraEnvironment(StrEnum):
 	prod = "prod"
 	stage = "stage"
